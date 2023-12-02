fc5c231e7fe9e7a41f31c8b9abfec6bed5357ab3
VJ <vishalj2023@outlook.com>
2023-12-02 23:58:15 +0530

    not

--- DBMS/SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-create view practicalview
-AS SELECT BusinessEntityId, name
-from TrialTable1
-
-select *
-from practicalview
-
-
-Drop view practicalview
--- DBMS/SQLQuery4.sql
+++
@@ -1,7 +0,0 @@
-create procedure pocedure1
-AS
-Begin
-  select * from TrialTable1 where SalesPersonId=281
-end
-
-exec pocedure1
