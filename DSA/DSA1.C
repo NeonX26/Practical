25040889d7244cb9e284fdf404dd10b58ee02b7e
VJ <vishalj2023@outlook.com>
2023-12-02 22:46:24 +0530

    .

---
+++ DSA/DSA1.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+ {
+    char st[10];
+    int n, i;
+    char *q;
+    char *ps;
+    clrscr();
+    ps = st;
+    q = ps;
+
+    printf("Enter a string:");
+
+    while ((*ps = getchar()) != '\n')
+     {
+	ps++;
+     }
+
+    n = ps - q;
+
+    printf("\nLength of string=%d", n);
+
+    --ps;
+
+    printf("\nString in reverse order is:");
+
+    for (i = 0; i < n; i++)
+    {
+	putchar(*ps);
+	ps--;
+    }
+
+    printf("\n");
+    getch();
+    return 0;
+}
